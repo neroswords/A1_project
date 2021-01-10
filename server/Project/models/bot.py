d524251cd34db1b2dae604236c13630cf4a86c26
jarukanno <jarukan.jeenyoo@gmail.com>
2021-01-10 21:44:45 +0700

    from nong noey

--- server/Project/models/bot.py
+++ server/Project/models/bot.py
@@ -1,16 +1,18 @@
 class ChatBot:
    
 
-    def __init__(self, username,name_bot, Channel_access_token, Channel_secret,VERIFY_TOKEN,basic_id,page_facebook_access_token):
-        self.username = username
-        self.name_bot = name_bot
-        self.Channel_access_token = Channel_access_token
-        self.Channel_secret = Channel_secret
+    def __init__(self, owner,bot_name, channel_access_token, channel_secret,VERIFY_TOKEN,basic_id,page_facebook_access_token,age,gender):
+        self.owner = owner
+        self.bot_name = bot_name
+        self.channel_access_token = Channel_access_token
+        self.channel_secret = channel_secret
         self.VERIFY_TOKEN = VERIFY_TOKEN
         self.basic_id = basic_id
         self.page_facebook_access_token = page_facebook_access_token
-
-    # def __init__(self, username, Channel_access_token, Channel_secret,VERIFY_TOKEN,basic_id,page_facebook_access_token):
+        self.age = age
+        self.gender = gender
+        
+            # def __init__(self, username, Channel_access_token, Channel_secret,VERIFY_TOKEN,basic_id,page_facebook_access_token):
     #     self.username = username
     #     self.Channel_access_token = Channel_access_token
     #     self.Channel_secret = Channel_secret
@@ -19,4 +21,4 @@ class ChatBot:
     #     self.page_facebook_access_token = page_facebook_access_token
 
     def get_connect(self):
-        return self.username
+        return self.owner
--- server/Project/route/bot.py
+++ server/Project/route/bot.py
@@ -3,31 +3,94 @@ from flask import Flask, request, abort, render_template, session,url_for,redire
 from flask_login import LoginManager, login_user, logout_user, login_required,current_user,AnonymousUserMixin
 from pymessenger import Bot
 from Project.Config import *
+from Project.models.bot import ChatBot
 import json
 import requests
 from Project.message import ReplyMessage, process_message
 from Project.extensions import mongo
 from Project.nlp import sentence_get_confident
+from bson.objectid import Objectid # find by id
 
 bot = Blueprint("bot",__name__)
+bots_collection = mongo.db.bots
 
-@bot.route('/connect', methods=['GET', 'POST'])
-@login_required
-def connect():
+#create bot
+@bot.route('/create', methods=[ 'POST'])
+def create():
+   
+    # bots_collection = mongo.db.bots
+ 
+    
     if request.method == 'POST':
-        # print(current_user._id)
-        # username = current_user.username
-        # ch_sc = request.form.get('ch_sc')
-        # ch_ac_tk = request.form.get('ch_ac_tk')
-        # basic_id = request.form.get('basic_id')
-        # pfa_tk = request.form.get('pfa_tk')
-        # vf_tk = request.form.get('vf_tk')
-        # update_connect(username, ch_sc,ch_ac_tk,basic_id,pfa_tk,vf_tk)
-        return redirect(url_for('home'))
+        bot_info = request.get_json()
+        print(bot_info)
+        
+            bot_name = bot_info['name_bot']
+            chanel_secret = bot_info['ch_sc']
+            chanel_access_token = bot_info['ch_ac_tk']
+            basic_id = bot_info['basic_id']
+            fb_access_token = bot_info['pfa_tk']
+            verify_token = bot_info['vf_tk']
+            owner = bot_info['owner'] #ref id คนสร้างมาใส่ตัวแปรนี้
+            gender = bot_info['gender']
+            age = bot_info['age']
+            # image = bot_image['image']
+            
+            info_bot = {'bot_name': bot_name, ' chanel_secret':  chanel_secret, 'chanel_access_token': chanel_access_token, 'basic_id': basic_id, 'fb_access_token': fb_access_token , 'verify_token': verify_token, 'owner': owner, 'gender' : gender, 'age': age}
+            bots_collection.insert_one(info_bot)
+            return "add bot successfully"
+        return "add bot unsuccessfully"
+
+#edit
+@bot.route('/edit/<id>', methods=['GET', 'POST'])
+def edit(id):
+    if request.method == 'POST':
+
+        bot_update = request.get_json()
+
+            bot_name = bot_update['name_bot']
+            chanel_secret = bot_update['ch_sc']
+            chanel_access_token = bot_update['ch_ac_tk']
+            basic_id = bot_update['basic_id']
+            fb_access_token = bot_update['pfa_tk']
+            verify_token = bot_update['vf_tk']
+
+
+            info_update = { "$set": {'bot_name': bot_name, ' chanel_secret':  chanel_secret, 'chanel_access_token': chanel_access_token, 'basic_id': basic_id, 'fb_access_token': fb_access_token , 'verify_token': verify_token}}
+            # bot_id = { "_id": ObjectId (id)}
+            done = bots_collection.update_one({'_id': ObjectId (id)}, info_update)
+            if done:
+                return "Update successfully"
+            else:
+                return "Update unsuccessfully"
+
     elif request.method == 'GET':
-        # gg = find_bot(current_user.username)
-        # print(gg[0]["name_bot"])
-        return render_template('connect.html')
+       bot =  bots_collection.find_one({'_id': ObjectId (id)}) # กุเขียนมั่ว
+        if bot:
+            bot_info = ChatBot(bot['owner'], bot['bot_name'], bot['chanel_access_token'], bot['chanel_secret'], bot['verify_token'], bot['basic_id'], bot['fb_access_token'],bot['age'],bot['gender'])
+       
+            return bot_info
+        else: 
+            return "Nope"
+
+
+ #delete
+@bot.route('/delete/<id>', methods=['POST'])
+def delete(id):
+    if request.method == 'POST':
+        if  bots_collection.find_one({'_id': ObjectId(id)}):
+            result = bots_collection.delete_one({'_id': ObjectId(id)})
+            if result:
+                return "delete successfully"
+            else:
+                return "delete unsuccessfully"
+        else: 
+            return "There is no this bot in database"
+        
+
+
+
+        
 
 @bot.route('/<id>/add_message',methods=["POST"])
 def add_sentence(id):
