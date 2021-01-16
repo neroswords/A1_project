a85686c485c626892f0a96c7b57ce167aa969761
nampunpaniss <nampun.melody@gmail.com>
2021-01-16 21:08:57 +0700

    edit botton on navbar

--- client/src/page/Login.js
+++ client/src/page/Login.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
+// import {bg-login} from './images/bg-login.png';
+
 
 const Styles = styled.div`
+  .page {
+        background: url(${process.env.PUBLIC_URL +'/images/bg-login.png'}) no-repeat fixed bottom;
+  }
+
   .container {
     font-family: 'Public Sans', sans-serif;
   }
@@ -140,6 +146,7 @@ class Login extends React.Component {
   render(){
       return(
           <Styles>
+            <div className="page">
                 <div class="container">
                       <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
                         <div class="card card-signin my-5">
@@ -169,6 +176,7 @@ class Login extends React.Component {
                           </div>
                         </div>
                       </div>
+                  </div>
                   </div>
           </Styles>
     );
